05e642d2ed122bc57880dcc8e14c57472d86f509
Danbethwel <93147941+Danbethwel@users.noreply.github.com>
2023-07-24 13:29:20 +0300

    new commit

---
+++ js/loader.js
@@ -0,0 +1,6 @@
+// JavaScript
+// Wait for the entire page to load before running the JavaScript code
+window.addEventListener("load", function () {
+    // Add the "loaded" class to the body element to hide the preloader
+    document.body.classList.add("loaded");
+  });
